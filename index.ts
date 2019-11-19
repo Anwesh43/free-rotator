b6c72c0f855fc3194d92fba7b72d7d4f3961840a
Anwesh43 <anweshthecool0@gmail.com>
2019-11-19 16:30:44 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const w : number = window.innerWidth
-const h : number = window.innerHeight
+var w : number = window.innerWidth
+var h : number = window.innerHeight
 const loopDelay : number = 30
 const circleSizeFactor : number = 4
 const arrowSizeFactor : number = 9
@@ -10,3 +10,39 @@ const circleColor : string = "#3F51B5"
 const arrowColor : string = "#E0E0E0"
 const ballColor : string = "#f44336"
 const timerColor : string = "white"
+
+window.onresize = () => {
+    w = window.innerWidth
+    h = window.innerHeight
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
