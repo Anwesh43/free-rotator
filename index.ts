b94d389880289a40809f7ef1a910fa8c5b056dac
Anwesh43 <anweshthecool0@gmail.com>
2019-11-19 17:01:28 +0530

    adding Loop and LoopObjects

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 var w : number = window.innerWidth
 var h : number = window.innerHeight
-const loopDelay : number = 30
+const loopDelay : number = 1
 const circleSizeFactor : number = 4
 const arrowSizeFactor : number = 9
 const ballSizeFactor : number = 7
@@ -10,6 +10,8 @@ const circleColor : string = "#3F51B5"
 const arrowColor : string = "#E0E0E0"
 const ballColor : string = "#f44336"
 const timerColor : string = "white"
+const arrowDelay : number = 50
+var ballCreateDelay : number = 1000
 
 window.onresize = () => {
     w = window.innerWidth
@@ -44,5 +46,66 @@ class Stage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class LoopObject {
+
+    constructor(public t : number, public startTime : number, public cb : Function, public delay : number) {
+
+    }
+
+    execute(currTime : number) {
+        if (this.cb && typeof this.cb === "function" && currTime - this.startTime >= this.delay) {
+            this.startTime = currTime
+            this.cb()
+        }
+    }
+}
+
+class Loop {
+
+    interval : number
+    loopObjects : Array<LoopObject> = new Array()
+    started : boolean = false
+    n : number = 0
+
+    start() {
+        if (!this.started) {
+            this.started = true
+            this.interval = setInterval(() => {
+                this.execute()
+            }, loopDelay)
+        }
+    }
+
+    add(cb : Function, delay : number) {
+        this.n++
+        const time = new Date().getTime()
+        const loopObject = new LoopObject(this.n, time, cb, delay)
+    }
+
+    stop() {
+        if (this.started) {
+            this.started = false
+            clearInterval(this.interval)
+        }
+    }
+
+    execute() {
+        if (this.started) {
+            this.loopObjects.forEach((loopObject) => {
+                loopObject.execute(new Date().getTime())
+            })
+        }
+    }
+
+    remove(n : number) {
+        for (var i = this.loopObjects.length - 1; i >= 0; i--) {
+            const loopObject = this.loopObjects[i]
+            if (loopObject.t == n) {
+                this.loopObjects.splice(i, 1)
+            }
+        }
     }
 }
